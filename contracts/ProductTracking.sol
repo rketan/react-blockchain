c0cfb3463111126903fc832fa3840d3b5b2a1568
Rutvi Thakkar <rthakkar@Rutvis-MacBook-Air.local>
2022-05-09 14:54:09 -0700

    [Contracts] Role contracts

--- contracts/ProductTracking.sol
+++ contracts/ProductTracking.sol
@@ -1,6 +1,11 @@
 pragma solidity >=0.4.22 <0.9.0;
 
-contract ProductTracking {
+import './accessControl/DistributorRole.sol';
+import './accessControl/ManufacturerRole.sol';
+import './accessControl/VendorRole.sol';
+import './core/Ownable.sol';
+
+contract ProductTracking is Ownable, ManufacturerRole, DistributorRole, VendorRole {
     // Model a Product
     struct Product {
         uint id;
@@ -8,16 +13,26 @@ contract ProductTracking {
         string currentStatus;
     }
 
-    event votedEvent (
-        uint indexed _productId
-    );
-
-    // Store accounts that have voted
-    mapping(address => bool) public voters;
+    // Track the journey
+    mapping (uint => string[]) productsHistory;
     // Read/write products
     mapping(uint => Product) public products;
     // Store Products Count
     uint public productsCount;
+
+    // Define enum 'State' with the following values:
+    enum State
+    {
+        Manufactured,  // 0
+        OrderPlaced,  // 1
+        Shipped,     // 2
+        DistRecieved,    // 3
+        InTransit,       // 4
+        VendorRecieved,    // 5
+        Purchased   // 6
+    }
+
+    State constant defaultState = State.Manufactured;
 
     constructor() public {
         addProduct("Product 1");
@@ -34,15 +49,8 @@ contract ProductTracking {
     }
 
     function vote (uint _productId) public {
-    // require that they haven't voted before
-        // TODO: recheck how to handle this
-        //require(!voters[msg.sender]);
-
         // require a valid product
         require(_productId > 0 && _productId <= productsCount);
-
-        // record that product has been updated
-        voters[msg.sender] = true;
 
         // update product status
         if(compareStrings(products[_productId].currentStatus, "IN-WAREHOUSE")) {
@@ -50,9 +58,6 @@ contract ProductTracking {
         }else if(compareStrings(products[_productId].currentStatus, "IN-TRANSIT")) {
             products[_productId].currentStatus = 'DISPATCHED';
         }
-
-        // trigger update event
-        emit votedEvent(_productId);
     }
     
     function compareStrings (string memory a, string memory b) public view returns (bool) {
